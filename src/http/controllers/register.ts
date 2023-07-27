96b768dc8218f639b8ae578e23b65ffb447f47e5
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-07-27 10:49:37 -0300

    ✨ feat: Create http routes and first register controller

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+import { RegisterUseCase } from '@/use-cases/register';
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error';
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const prismaUserRepository = new PrismaUserRepository();
+    const registerUseCase = new RegisterUseCase(prismaUserRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error;
+  }
+
+  return reply.status(201).send();
+}
+
